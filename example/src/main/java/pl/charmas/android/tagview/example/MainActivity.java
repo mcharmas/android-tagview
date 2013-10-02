2721f8942723f1ad4f702f9546b2c8fadd722142
Michał Charmas <michal@charmas.pl>
2013-10-02 19:12:21 +0200

    Removed unecessary files. Added licence header to all the files.

--- example/src/main/java/pl/charmas/android/tagview/example/MainActivity.java
+++ example/src/main/java/pl/charmas/android/tagview/example/MainActivity.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2013 Michał Charmas
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package pl.charmas.android.tagview.example;
 
 import android.content.res.TypedArray;
@@ -7,7 +22,6 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.ViewParent;
 
 import java.util.LinkedList;
 
